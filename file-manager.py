710c9760771f43c575632a23a7fda23d71fd8f3f
ivica <ivica.beljo@gmail.com>
2025-05-30 18:15:43 +0200

    File-manager

---
+++ file-manager.py
@@ -0,0 +1,26 @@
+import os
+availble_commands = {"pwd":"Working directory", "cd..":"Change directory"}
+# run the user's program in our generated folders
+os.chdir('module/root_folder')
+def get_command(str):
+    if str == "pwd":
+        print(os.getcwd())
+    elif str == "cd ..":
+        split = os.path.split(os.getcwd())
+        print(split[1])
+        os.chdir(split[0])
+    elif str.startswith("cd"):
+        path = str[3:]
+        os.chdir(os.path.join(os.getcwd(),path))
+        print(path)
+    else:
+        print("Invalid command")
+
+command = input()
+while command != "quit":
+    try:
+        get_command(command)
+        command = input()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        break
